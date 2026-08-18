db8815d571dea12d40361a1c144e33ea145af20e
Davut Eren Ekici <davuteren052@gmail.com>
2026-08-18 21:49:07 +0300

    Sending data and receiving responses via API.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const difficultySelect = document.querySelector("#difficulty");
 
 
 
-startQuiz.addEventListener("click",function(){
+startQuiz.addEventListener("click", async function(){
     console.log("Quiz Başlatıldı!");
 
     const topic = topicInput.value;
@@ -18,10 +18,32 @@ startQuiz.addEventListener("click",function(){
       difficulty
     };
 
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts",{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(quizSettings)
+    });
+      
+
+    
 
     const quizJSON = JSON.stringify(quizSettings);
     const quizObject = JSON.parse(quizJSON);
     console.log(quizObject);
     console.log(quizJSON);
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
    
 });
+
+async function testAPI() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  console.log(response);
+  const data = await response.json();
+  console.log(data);
+  console.log(data.title);
+}
+testAPI();
