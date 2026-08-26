99c08f3fd29f800d20c29cd9a4cc7f5d0cfa2017
Davut Eren Ekici <davuteren052@gmail.com>
2026-08-26 23:51:48 +0300

    feat: connect frontend to local backend with POST /generate-quiz and add cors middleware

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const difficultySelect = document.querySelector("#difficulty");
 
 
 
-startQuiz.addEventListener("click", async function(){
+startQuiz.addEventListener("click", async function() {
     console.log("Quiz Başlatıldı!");
 
     const topic = topicInput.value;
@@ -13,30 +13,21 @@ startQuiz.addEventListener("click", async function(){
     const difficulty = difficultySelect.value;
 
     const quizSettings = {
-      topic,
-      questionCount,
-      difficulty
+        topic,
+        questionCount,
+        difficulty
     };
 
-    const response = await fetch("https://jsonplaceholder.typicode.com/posts",{
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(quizSettings)
+    const response = await fetch("http://localhost:3000/generate-quiz", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(quizSettings)
     });
-      
 
-    
-
-    const quizJSON = JSON.stringify(quizSettings);
-    const quizObject = JSON.parse(quizJSON);
-    console.log(quizObject);
-    console.log(quizJSON);
-    console.log(response);
     const data = await response.json();
-    console.log(data);
-   
+    console.log("Backend'den gelen cevap:", data);
 });
 
 async function testAPI() {
--- server.js
+++ server.js
@@ -1,2 +1,23 @@
 const express = require("express");
+
 const app = express();
+
+app.use(express.json());
+
+
+app.post("/generate-quiz", function(req,res) {
+    console.log(req.body);
+
+    res.json({
+        message: "Quiz ayarları backend'e ulaştı!",
+        quizSettings: req.body
+    });
+});
+
+app.get("/" , function(req, res) {
+    res.send("AI Quiz Backend çalışıyor!");
+});
+
+app.listen(3000, function(){
+    console.log("Server çalışıyor : http://localhost:3000");
+});
